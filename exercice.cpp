66217f129ce783a49f4ebef5aa4ba89f4f29db0e
abigailmarkwilliam2 <117717727+abigailmarkwilliam2@users.noreply.github.com>
2022-11-16 17:34:03 +0100

    Add files via upload

---
+++ exercice.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{/*int n=5;
+
+	//printf("entrer n:"); 
+	//scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("n est paire");
+	}
+	else
+	{
+		printf("n n'est pas paire");
+	}*/
+	
+/*	if(n<1)
+	{
+		printf("desole!!!! tu n'es pas ne");
+	}
+	else if(n==10)
+	{
+		printf("tu as 10 ans");
+	}
+	else
+	{
+		
+	}printf("age: %d",n);
+	
+	 do
+	 {
+		printf("les zombies nous envahissent\n"); 
+		n++;
+	}
+	while(n<10);	*/
+	
+	return 0;
+}
